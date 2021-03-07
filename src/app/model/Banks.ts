fa6eaec0ce0795ade0d24595892e29528e2c123b
Aleksandr <maked0nskihtc@gmail.com>
2021-03-07 19:43:57 +0200

    add Class Clients and Banks

---
+++ src/app/model/Banks.ts
@@ -0,0 +1,9 @@
+export class Banks {
+  idMFO: number;
+  NameBank: string;
+
+  constructor(idMFO: number, NameBank: string) {
+    this.idMFO = idMFO;
+    this.NameBank = NameBank;
+  }
+}
---
+++ src/app/model/Client.ts
@@ -0,0 +1,35 @@
+import {Banks} from './Banks';
+
+export class Client {
+  id: number;
+  name: string;
+  legalAdress: string;
+  actualAdress: string;
+  company: boolean;
+  telephone: number;
+  edrpou: number;
+  ipn: number;
+  iban: number;
+  bank: Banks;
+  director: string;
+  surveillance: boolean;
+  maintenance: boolean;
+
+
+  // tslint:disable-next-line:max-line-length
+  constructor(id: number, name: string, legalAdress: string, actualAdress: string, company: boolean, telephone: number, edrpou: number, ipn: number, iban: number, bank: Banks, director: string, surveillance: boolean, maintenance: boolean) {
+    this.id = id;
+    this.name = name;
+    this.legalAdress = legalAdress;
+    this.actualAdress = actualAdress;
+    this.company = company;
+    this.telephone = telephone;
+    this.edrpou = edrpou;
+    this.ipn = ipn;
+    this.iban = iban;
+    this.bank = bank;
+    this.director = director;
+    this.surveillance = surveillance;
+    this.maintenance = maintenance;
+  }
+}
